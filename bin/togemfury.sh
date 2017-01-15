e08897bbb88fd5751f735d91c4c2e99fd0030941
Horia Coman <horia141@gmail.com>
2017-01-15 20:58:02 +0200

    Add debugging info to gemfury.

--- bin/togemfury.sh
+++ bin/togemfury.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+ls -lah
+
 npm pack
 mv `ls identity-*.tgz` identity.tgz
-curl -F package=@identity.tgz https://vsRAKKMwEs5p1RhfMGiF@push.fury.io/neoncity/ > result
+curl -v -F package=@identity.tgz https://vsRAKKMwEs5p1RhfMGiF@push.fury.io/neoncity/ > result
 if [ -z "$(grep -e ok result)" ]
 then
     rm result
