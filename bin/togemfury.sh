3a08e4b850fbaf02a30f7458db4ffcc3d5a416c7
Horia Coman <horia141@gmail.com>
2017-01-15 20:23:08 +0200

    Add gemfury push

---
+++ bin/togemfury.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm pack
+curl -s -F package=@`ls identity-*.tgz` https://vsRAKKMwEs5p1RhfMGiF@push.fury.io/ocelot-saas/ > result
+if [ -z "$(grep -e ok result)" ]
+then
+    rm result
+    exit 1
+fi
+rm result
+
