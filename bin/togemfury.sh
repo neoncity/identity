6369efa25cf7e26de426ac8a8305277d73dc42d0
Horia Coman <horia141@gmail.com>
2017-01-15 21:04:15 +0200

    Revert to original gemfury upload

--- bin/togemfury.sh
+++ bin/togemfury.sh
@@ -3,8 +3,7 @@
 ls -lah
 
 npm pack
-mv `ls identity-*.tgz` identity.tgz
-curl -v -F package=@identity.tgz https://vsRAKKMwEs5p1RhfMGiF@push.fury.io/neoncity/ > result
+curl -s -F package=@`ls identity-*.tgz` https://vsRAKKMwEs5p1RhfMGiF@push.fury.io/neoncity/ > result
 if [ -z "$(grep -e ok result)" ]
 then
     rm result
