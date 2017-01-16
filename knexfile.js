3121c0d4126e48d77dcfab000e570840eeba72fe
Horia Coman <horia141@gmail.com>
2017-01-16 18:07:04 +0200

    Force ssl connection to postgresql.

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
 	client: 'pg',
-	connection: process.env.DATABASE_URL,
+	connection: process.env.DATABASE_URL + '?sslmode=require',
 	pool: {
 	    min: 2,
 	    max: 10
