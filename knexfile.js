f6da6c965da8e815500794fe09c371696e8bd076
Horia Coman <horia141@gmail.com>
2017-01-16 18:15:04 +0200

    Another attempt at a knexfile confi.

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,12 @@
 module.exports = {
-    development: {
-	client: 'pg',
-	connection: process.env.DATABASE_URL + '?sslmode=require',
-	pool: {
-	    min: 2,
-	    max: 10
-	},
-	migrations: {
-	    directory: process.env.DATABASE_MIGRATIONS_DIR,
-	    tableName: process.env.DATABASE_MIGRATIONS_TABLE
-	}
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?sslmode=require',
+    pool: {
+	min: 2,
+	max: 10
+    },
+    migrations: {
+	directory: process.env.DATABASE_MIGRATIONS_DIR,
+	tableName: process.env.DATABASE_MIGRATIONS_TABLE
     }
 }
